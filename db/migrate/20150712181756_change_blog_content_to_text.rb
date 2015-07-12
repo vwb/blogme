d3e9ab30259216e1bfb95547b9b80fac2eed7585
Vincent Budrovich <vwb@berkeley.edu>
2015-07-12 20:21:28 +0200

    Post Content to Text

---
+++ db/migrate/20150712181756_change_blog_content_to_text.rb
@@ -0,0 +1,5 @@
+class ChangeBlogContentToText < ActiveRecord::Migration
+  def change
+  	change_column :posts, :content, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710173911) do
+ActiveRecord::Schema.define(version: 20150712181756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 20150710173911) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.string   "title"
-    t.string   "content"
+    t.text     "content"
     t.string   "location"
     t.integer  "blog_id"
   end
