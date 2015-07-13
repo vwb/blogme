a452364a08ca9d5b80fad5cc7290090b1abbd580
Vincent Budrovich <vwb@berkeley.edu>
2015-07-13 22:22:51 +0200

    No Sign In for Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-	before_filter :authenticate_user!
+	#before_filter :authenticate_user!
 
 	def new
 		@comment = Comment.new
@@ -12,7 +12,9 @@ class CommentsController < ApplicationController
 	def create
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.create(comment_params)
-		@comment.user_id = current_user.id
+		if user_signed_in?
+			@comment.user_id = current_user.id
+		end
 		if @comment.save
 			redirect_to post_path(@post)
 		else
@@ -35,7 +37,7 @@ class CommentsController < ApplicationController
 	private
 
 	def comment_params
-		params.require(:comment).permit(:content, :user_id)
+		params.require(:comment).permit(:content, :user_id, :name)
 	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713125935) do
+ActiveRecord::Schema.define(version: 20150713194831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20150713125935) do
     t.string   "content"
     t.integer  "post_id"
     t.integer  "user_id"
+    t.string   "name"
   end
 
   create_table "posts", force: true do |t|
