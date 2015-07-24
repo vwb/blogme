8deaee5eb4fb48d300f96e3a6017f33b65270c11
Vincent Budrovich <vwb@berkeley.edu>
2015-07-24 13:01:02 +0200

    sendgrid config, fixed error again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,15 +78,15 @@ Rails.application.configure do
 
   Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['SEARCHBOX_URL']
 
-  config.action_mailer.default_url_options = {host="www.blogmenow.com"}
-  config.action_mailer.deliver_method = :stmp
+  config.action_mailer.default_url_options = {host: "www.blogmenow.heroku.com"}
+  config.action_mailer.deliver_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 
